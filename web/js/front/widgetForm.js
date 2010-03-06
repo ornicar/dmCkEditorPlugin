154d5f05431cbeaa066f3da91271b16d607ffdad
Vincent Jousse <vincent.jousse@devorigin.fr>
2010-03-06 15:27:30 +0100

    Fixed reload of ckeditor with ajax request

--- web/js/front/widgetForm.js
+++ web/js/front/widgetForm.js
@@ -1,17 +1,29 @@
 $.fn.dmWidgetContentCkEditorForm = function(widget)
 {
-  var self = this, $textarea = self.find('textarea.dm_ckeditor'), editor;
 
-  $textarea.ckeditor(function()
-  {
-    $.dbg(this);
-  },
-  $textarea.metadata()
-  );
+    //Kill all existing instances before loading
+    //ckeditor again or it will not work with ajax request
+    for(var name in CKEDITOR.instances)
+    {
+        CKEDITOR.remove(CKEDITOR.instances[name]);
+    }
 
-  self.find('input.submit').click(function() {
-    $textarea.text($textarea.val());
-  });
+
+    var self = this, $textarea = self.find('textarea.dm_ckeditor'), editor;
+
+    $textarea.ckeditor(function()
+        {
+            $.dbg(this);
+        },
+        $textarea.metadata()
+    );
+
+
+    self.find('input.submit').click(function() {
+
+        $textarea.text($textarea.val());
+
+    });
 };
 
 window.CKEDITOR_BASEPATH = dm_configuration.relative_url_root+'/dmCkEditorPlugin/js/ckeditor/';
