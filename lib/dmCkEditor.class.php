9605d5ff74522d8030e49b39201d533c0890184d
Robert Gruendler <r.gruendler@gmail.com>
2010-03-07 12:40:06 +0100

    added support for page dragging

--- lib/dmCkEditor.class.php
+++ lib/dmCkEditor.class.php
@@ -29,19 +29,55 @@ class dmCkEditor
     {
       $this->updateImage($image);
     }
-    return $html;
     
+    foreach ($html->find('a.link') as $page) 
+    {
+      $this->updatePage($page);
+    }
+    return $html;
   }
 
   /**
+   * @param string $page link to the page to update
+   * @return void
+   */
+  protected function updatePage($page) {
+    
+    $id = str_replace('dmPage-','',$page->id);
+    if (!$id || !is_numeric($id))
+      return;
+      
+    $_page = Doctrine_Core::getTable('DmPage')
+      ->createQuery('p')
+      ->innerJoin('p.Translation t')
+      ->where('p.id = ?', $id)
+      ->fetchOne();
+    
+    if (!$_page)
+      return;
+      
+    $url = $this->helper->link($_page)->getHref();
+
+    if ($page->href != $url) 
+    {
+      $page->href = $url;
+    }
+  }
+  
+  /**
    * 
    * @param string $image
+   * @return void
    */
   protected function updateImage($image) 
   {
     
     $id = str_replace('ck-media-','',$image->id);
+    if (!$id || !is_numeric($id))
+      return;
     $media = Doctrine_Core::getTable('dmMedia')->findOneById($id);
+    if (!$media)
+      return;
     $src = $this->helper->media($media)->getSrc();
     if ($image->src != $src) 
     {
--- modules/dmMedia/templates/indexSuccess.php
+++
@@ -1,4 +0,0 @@
-<?php
-  echo £media($media)->set('#ck-media-' . $media->getId());
-  
-//  echo get_class(sfContext::getInstance()->get('markdown'));
