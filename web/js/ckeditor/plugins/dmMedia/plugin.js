c4d360b8edd995f982037634f12d6141492a8cc5
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-03-07 14:38:25 +0100

    added the ckeditor dmMedia plugin by default, fixed media url when site does not live in the domain root

--- web/js/ckeditor/plugins/dmMedia/plugin.js
+++ web/js/ckeditor/plugins/dmMedia/plugin.js
@@ -6,21 +6,21 @@
 function createOverlay(editor, link) {
 
   var overlayid = 'drag_box_' + editor.name;
-	$editor = $('#cke_' + editor.name);
-	var offset = $editor.offset();
-	$(document.body).append('<div id="'+overlayid+'"><input style="width: 100%; height: 100%;"></input></div>');
-	 
-	$('#' + overlayid).css({
-		position: 'absolute',
-		left: offset.left + 'px',
-		top: offset.top + 'px',
-		height: $editor.height() + 'px',
-		width: $editor.width() + 'px',
-		backgroundColor: 'white',
-		opacity: 0.5,
-		zIndex: 1000,
-		display: 'block'
-	}).find('input:first').dmDroppableInput(function() {
+  $editor = $('#cke_' + editor.name);
+  var offset = $editor.offset();
+  $(document.body).append('<div id="'+overlayid+'"><input style="width: 100%; height: 100%;"></input></div>');
+
+  $('#' + overlayid).css({
+    position: 'absolute',
+    left: offset.left + 'px',
+    top: offset.top + 'px',
+    height: $editor.height() + 'px',
+    width: $editor.width() + 'px',
+    backgroundColor: 'white',
+    opacity: 0.5,
+    zIndex: 1000,
+    display: 'block'
+  }).find('input:first').dmDroppableInput(function() {
     $.ajax({
       url: link + $(this).val().split(' ')[0].split(':')['1'],
       success: function(src) {
@@ -28,7 +28,7 @@ function createOverlay(editor, link) {
       }
     });
   });
-	return overlayid;
+  return overlayid;
 }
 
 CKEDITOR.plugins.add('dmMedia',
@@ -36,23 +36,23 @@ CKEDITOR.plugins.add('dmMedia',
   init: function(editor)
   {
 
-		CKEDITOR.instances[editor.name].on('instanceReady', function() {
+    CKEDITOR.instances[editor.name].on('instanceReady', function() {
       var overlayid;
-	    $('.image.ui-draggable').live('dragstart', function(event, ui) {
-        overlayid = createOverlay(editor, '/admin.php/+/dmMedia/index/id/');			 	
+      $('.image.ui-draggable').live('dragstart', function(event, ui) {
+        overlayid = createOverlay(editor, $.dm.ctrl.getHref('+/dmMedia/index/id/'));
       });
       
-			$('#dm_page_tree li.ui-draggable').live('dragstart', function(event, ui){
-				overlayid = createOverlay(editor, '/admin.php/+/dmMedia/page/id/');
-			});
-			
+      $('#dm_page_tree li.ui-draggable').live('dragstart', function(event, ui){
+        overlayid = createOverlay(editor, $.dm.ctrl.getHref('+/dmMedia/page/id/'));
+      });
+
       $('#dm_page_tree li.ui-draggable').live('dragstop', function(event, ui) {
         $('#' + overlayid).remove();
       });
-			
+
       $('.image.ui-draggable').live('dragstop', function(event, ui) {
         $('#' + overlayid).remove();
       });
-		});
+    });
   }
 });
